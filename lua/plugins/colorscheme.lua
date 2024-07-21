15057efb7faa1b2069a259cb90ec46ee3c66e7e1
kedarmd <dindekedar@gmail.com>
2024-07-22 00:34:26 +0530

    feat: completed initial vim setup

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,28 @@
+return {
+    -- "AlexvZyl/nordic.nvim",
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --     local palette = require("nordic.colors")
+    --
+    --     require("nordic").setup({
+    --         transparent_bg = true,
+    --         telescope = { style = "classic" },
+    --         override = {
+    --             CursorLine = {
+    --                 bg = "#2e3440",
+    --                 bold = true,
+    --             },
+    --         },
+    --     })
+    --
+    --     vim.cmd("colorscheme nordic")
+    --     vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "#1e2022" })
+    -- end,
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        vim.cmd.colorscheme("catppuccin");
+    end
+}
